19bb90266076134c8dc94116d42fb2ecd9f878f8
raullxb <raullxb@gmail.com>
2026-04-07 20:12:06 -0300

    commit

---
+++ src/aula6/Clientes.java
@@ -0,0 +1,7 @@
+package aula6;
+
+public class Clientes {
+    public String name;
+    public String cpf;
+    public Float saldo;
+}
---
+++ src/aula6/Main.java
@@ -0,0 +1,23 @@
+class Clientes {
+    public String name;
+    public String cpf;
+    public Float saldo;
+}
+
+
+void main() {
+//    int numero1 = Integer.parseInt(IO.readln("Digite o primeiro valor: "));
+//    int numero2 = Integer.parseInt(IO.readln("Digite o segundo valor: "));
+//
+//    if (numero1 > numero2) {
+//        IO.println("O numero 1 é maior");
+//    } else if (numero2 > numero1) {
+//        IO.println("O numero 2 é maior");
+//    } else {
+//        IO.println("Os numeros são iguais");
+//    }
+//
+
+
+}
+
